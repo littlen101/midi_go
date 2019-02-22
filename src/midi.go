32d993dab4480f04f119b99de957b556c8fb33f2
littlen101 <nlewis2016@my.fit.edu>
2019-02-22 16:07:00 -0600

    Created noteOn, noteOff functions

--- src/midi.go
+++ src/midi.go
@@ -18,6 +18,11 @@ type NoteStack struct {
 	s []PartialNote
 }
 
+// How big is the stack
+func (ns *NoteStack) length() int {
+	return len(ns.s)
+}
+
 // adds element to the tail of slice || top of stack
 // [a0, a1, a2, ...., an-1], an -> [a0, a1, a2, ...., an-1, an]
 func (ns *NoteStack) push(pnote PartialNote) {
@@ -41,12 +46,271 @@ type PartialNote struct {
 	track, channel int
 }
 
+// Compare Interface of PartialNotes
+func (pn *PartialNote) compare(other PartialNote) bool {
+	return other.midiNumber == pn.midiNumber
+}
+
 // A full note that has a beginning tick and ending tick
 type Note struct {
 	start, end     int // The start and ending ticks to determine duration and order
 	midiNumber     int // (A|B|C ..)(octave) needed for searching
 	track, channel int //octave seem synonymous with the pitch
 	// Comparing it to B3 and C4, which are simultaneous events, B3 is always before C4
+}
+
+// Goes into midiNote map and takes the note = index 0
+func (n *Note) getNote() string {
+	// map -> [Note, Octave]
+	return midiNote[n.midiNumber][0]
+}
+
+// Goes into midiNote map and takes the octave = index 1
+func (n *Note) getOctave() int {
+	// map -> [Note, Octave]
+	octave, _ := strconv.Atoi(midiNote[n.midiNumber][1])
+	return octave
+}
+
+// Gives the difference between the start and end times
+func (n *Note) duration() int {
+	return n.end - n.start
+}
+
+// Using the template format on project site constructs a string with the following format
+//ticks     note octave duration track channel
+func (n *Note) String() string {
+	return fmt.Sprintf("%7d:  %-4s %5d %5d %2d %2d",
+		n.start, n.getNote(), n.getOctave(), n.duration(), n.track, n.channel)
+}
+
+var notesOnStack NoteStack
+var notesOnContainer NoteStack
+
+func main() {
+	notesOnStack = NoteStack{make([]PartialNote, 0)}
+	notesOnContainer = NoteStack{make([]PartialNote, 0)}
+
+	file, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Get hex string comprised of file contents
+	hexMIDI := hex.EncodeToString(file)
+	tracks, format := getTracks(hexMIDI)
+	for i := 0; i < len(tracks); i++ {
+		trackNumber := i + 1
+		if format == 1 {
+			trackNumber++
+		}
+		getEvents(tracks[i], trackNumber)
+	}
+
+	stripNoteMeta("903e40")
+}
+
+func getEvents(track string, trackNumber int) {
+	track = track[8*2:] //the first four bytes are the track header and don't contain events
+	//We only care about note on and off
+	currentTime := 0
+	deltaTime, length := getVariableLengthNumber(track)
+	currentTime += deltaTime
+	track = track[length*2:]
+	eventData, eventLength := getEvent(track)
+	for eventData != trackEnd {
+		fmt.Println(eventData)
+		track = track[eventLength*2:]
+		if eventLength == 3 {
+			firstChar := eventData[0:1]
+			if firstChar == "8" {
+				noteOn(currentTime, eventData, trackNumber)
+			} else if firstChar == "9" {
+				noteOff(currentTime, eventData, trackNumber)
+			}
+		}
+		//For next event
+		deltaTime, length := getVariableLengthNumber(track)
+		currentTime += deltaTime
+		track = track[length*2:]
+		eventData, eventLength = getEvent(track)
+	}
+	/*
+		for each event
+			get delta time
+			trim delta time from event index indicates the last byte within the delta time
+	*/
+}
+
+func stripNoteMeta(data string) (int, int) {
+	// Take bit 1 and + 1 to get channel number
+	x, _ := strconv.ParseInt(data[1:2], 10, 32)
+	channel := x + 1
+
+	// Get MIDI Note number
+	midiNumber, _ := strconv.ParseUint(data[2:4], 16, 32)
+
+	return int(channel), int(midiNumber)
+}
+
+func noteOff(currentTime int, data string, trackNumber int) Note {
+	// Create a partial note for the end
+	channel, midiNumber := stripNoteMeta(data)
+	endNote := PartialNote{currentTime, midiNumber, trackNumber, channel}
+
+	// Go through the stack until you find a corresponding on note
+	cNote := notesOnStack.pop()
+	for !endNote.compare(cNote) {
+		notesOnContainer.push(cNote)
+		cNote = notesOnStack.pop()
+	}
+
+	note := Note{cNote.start, endNote.start, endNote.midiNumber,
+		endNote.track, endNote.channel}
+
+	// Place notes back into stack in order
+	for i := 0; i < notesOnContainer.length(); i++ {
+		bNote := notesOnContainer.pop()
+		notesOnStack.push(bNote)
+	}
+
+	return note
+}
+
+func noteOn(currentTime int, data string, trackNumber int) {
+	channel, midiNumber := stripNoteMeta(data)
+
+	cNote := PartialNote{currentTime, midiNumber, trackNumber, channel}
+
+	notesOnStack.push(cNote)
+}
+
+func getEvent(track string) (string, int) {
+	firstByte := substring(track, 0, 1)
+	var eventLength int
+	switch firstByte {
+	case "f0":
+		fallthrough
+	case "f1":
+		eventLength = getEventLength(track, 2)
+	case "ff":
+		secondByte := substring(track, 1, 2)
+		switch secondByte {
+		case "20":
+			eventLength = 4
+		case "2f":
+			eventLength = 3
+		case "51":
+			eventLength = 6
+		case "54":
+			eventLength = 8
+		case "58":
+			eventLength = 7
+		case "00":
+			fallthrough
+		case "59":
+			eventLength = 5
+		default:
+			eventLength = getEventLength(track, 2)
+		}
+	default:
+		firstChar := firstByte[0:1]
+		if (firstChar == "c") || (firstChar == "d") {
+			eventLength = 2
+		} else {
+			eventLength = 3
+		}
+	}
+	return substring(track, 0, eventLength), eventLength
+}
+
+/*
+	Returns the total number of bytes an event uses
+*/
+func getEventLength(event string, varOffset int) int {
+	eventLength, length := getVariableLengthNumber(event[varOffset*2:])
+	return eventLength + length + varOffset
+}
+
+/*
+	Gets the variable-length number by implementing the algorithm described below
+	http://valentin.dasdeck.com/midi/midifile.htm
+	Each non-terminal byte will have a  1 as the leftmost bit.
+	All bytes will ignore the left most bit
+
+	returns the variable length number and its length in bytes
+*/
+func getVariableLengthNumber(track string) (int, int) {
+	index := 0
+	value := getInt(track, index, index+1)
+	if (value & 0x80) != 0 { //check if leftmost bit is 1
+		msb := value  //if it is 1, then //Most Significant Bit
+		value &= 0x7F //set it to 0
+		//Check the next byte we're adding
+		for msb&0x80 != 0 {
+			//Add it to the current value
+			index++
+			msb = getInt(track, index, index+1)
+			value = (value << 7) + msb&0x7F
+		}
+	}
+	return value, index + 1
+}
+
+/*
+	Returns an array of hex strings containing one track per string
+	If the MIDI file is in format 1, we ignore the first track
+*/
+func getTracks(hexMIDI string) ([]string, int) {
+	//Get information from the file header
+	length, format, numTracks := getHeaderInfo(hexMIDI)
+	fmt.Printf("Header Length: %d\nFormat: %d\nNumber of Tracks: %d\n", length, format, numTracks)
+	//We'll split the rest of the hex string into the tracks
+	//hexMIDI[(8+length)*2:)] gets rid of the header (length doesn't include the first 8 bytes
+	//Split after N uses the track end signifier to split the string into the remaining tracks
+	tracks := strings.SplitAfterN(hexMIDI[(8+length)*2:], trackEnd, numTracks)
+	//Format 0 specifies that only one track should exist
+	if format == 0 && len(tracks) != 1 {
+		fmt.Printf("Error\n")
+	} else if format == 1 {
+		//Format 1 says there's one or more tracks with an intial track containing the tempo info
+		//We don't need this info
+		tracks = tracks[1:]
+	}
+	//We don't need to do anything for format 2
+	return tracks, format
+}
+
+/* First character is index 0
+ * Includes start, excludes end
+ */
+func substring(s string, start, end int) string {
+	startIndex := start * 2
+	endIndex := end * 2
+	return s[startIndex:endIndex]
+}
+
+/*
+	Returns the integer specified by substring of the hex string
+*/
+func getInt(hexString string, start, end int) int {
+	num, err := strconv.ParseInt(substring(hexString, start, end), 16, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return int(num)
+}
+
+/*
+	Returns information stored in header according to midi specifications
+	https://www.csie.ntu.edu.tw/~r92092/ref/midi/
+*/
+func getHeaderInfo(hexString string) (int, int, int) {
+	//We could use the first four bytes to check if its a valid MIDI file
+	//But I'm assuming it will work
+	length := getInt(hexString, 4, 8)
+	format := getInt(hexString, 8, 10)
+	numTracks := getInt(hexString, 10, 12)
+	return length, format, numTracks
 }
 
 // A relation between the midi number and a given note on a keyboard (or not applicable)
@@ -179,233 +443,4 @@ var midiNote = map[int][]string{
 	2:   {"N/A", "99"},
 	1:   {"N/A", "99"},
 	0:   {"N/A", "99"},
-}
-
-// Goes into midiNote map and takes the note = index 0
-func (n *Note) getNote() string {
-	// map -> [Note, Octave]
-	return midiNote[n.midiNumber][0]
-}
-
-// Goes into midiNote map and takes the octave = index 1
-func (n *Note) getOctave() int {
-	// map -> [Note, Octave]
-	octave, _ := strconv.Atoi(midiNote[n.midiNumber][1])
-	return octave
-}
-
-// Gives the difference between the start and end times
-func (n *Note) duration() int {
-	return n.end - n.start
-}
-
-// Using the template format on project site constructs a string with the following format
-//ticks     note octave duration track channel
-func (n *Note) String() string {
-	return fmt.Sprintf("%7d:  %-4s %5d %5d %2d %2d",
-		n.start, n.getNote(), n.getOctave(), n.duration(), n.track, n.channel)
-}
-
-var notesOnStack NoteStack
-var notesOnContainer NoteStack
-
-func main() {
-	notesOnStack = NoteStack{make([]PartialNote, 0)}
-	notesOnContainer = NoteStack{make([]PartialNote, 0)}
-
-	file, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	//Get hex string comprised of file contents
-	hexMIDI := hex.EncodeToString(file)
-	tracks, format := getTracks(hexMIDI)
-	for i := 0; i < len(tracks); i++ {
-		trackNumber := i + 1
-		if format == 1 {
-			trackNumber++
-		}
-		getEvents(tracks[i], trackNumber)
-	}
-
-	stripNoteMeta("903e40")
-}
-
-func getEvents(track string, trackNumber int) {
-	track = track[8*2:] //the first four bytes are the track header and don't contain events
-	//We only care about note on and off
-	currentTime := 0
-	deltaTime, length := getVariableLengthNumber(track)
-	currentTime += deltaTime
-	track = track[length*2:]
-	eventData, eventLength := getEvent(track)
-	for eventData != trackEnd {
-		fmt.Println(eventData)
-		track = track[eventLength*2:]
-		if eventLength == 3 {
-			firstChar := eventData[0:1]
-			if firstChar == "8" {
-				noteOn(currentTime, eventData, trackNumber)
-			} else if firstChar == "9" {
-				noteOff(currentTime, eventData, trackNumber)
-			}
-		}
-		//For next event
-		deltaTime, length := getVariableLengthNumber(track)
-		currentTime += deltaTime
-		track = track[length*2:]
-		eventData, eventLength = getEvent(track)
-	}
-	/*
-		for each event
-			get delta time
-			trim delta time from event index indicates the last byte within the delta time
-	*/
-}
-
-func stripNoteMeta(data string) {
-	// Take bit 1 and + 1 to get channel number
-	x, _ := strconv.ParseInt(data[1:2], 10, 32)
-	channel := x + 1
-
-	// Get MIDI Note number
-	fmt.Println(strconv.ParseUint("37", 16, 32))
-	fmt.Println(channel)
-}
-
-func noteOff(currentTime int, data string, trackNumber int) {
-	//TODO
-}
-
-func noteOn(currentTime int, data string, trackNumber int) {
-	//
-}
-
-func getEvent(track string) (string, int) {
-	firstByte := substring(track, 0, 1)
-	var eventLength int
-	switch firstByte {
-	case "f0":
-		fallthrough
-	case "f1":
-		eventLength = getEventLength(track, 2)
-	case "ff":
-		secondByte := substring(track, 1, 2)
-		switch secondByte {
-		case "20":
-			eventLength = 4
-		case "2f":
-			eventLength = 3
-		case "51":
-			eventLength = 6
-		case "54":
-			eventLength = 8
-		case "58":
-			eventLength = 7
-		case "00":
-			fallthrough
-		case "59":
-			eventLength = 5
-		default:
-			eventLength = getEventLength(track, 2)
-		}
-	default:
-		firstChar := firstByte[0:1]
-		if (firstChar == "c") || (firstChar == "d") {
-			eventLength = 2
-		} else {
-			eventLength = 3
-		}
-	}
-	return substring(track, 0, eventLength), eventLength
-}
-
-/*
-	Returns the total number of bytes an event uses
-*/
-func getEventLength(event string, varOffset int) int {
-	eventLength, length := getVariableLengthNumber(event[varOffset*2:])
-	return eventLength + length + varOffset
-}
-
-/*
-	Gets the variable-length number by implementing the algorithm described below
-	http://valentin.dasdeck.com/midi/midifile.htm
-	Each non-terminal byte will have a  1 as the leftmost bit.
-	All bytes will ignore the left most bit
-
-	returns the variable length number and its length in bytes
-*/
-func getVariableLengthNumber(track string) (int, int) {
-	index := 0
-	value := getInt(track, index, index+1)
-	if (value & 0x80) != 0 { //check if leftmost bit is 1
-		msb := value  //if it is 1, then //Most Significant Bit
-		value &= 0x7F //set it to 0
-		//Check the next byte we're adding
-		for msb&0x80 != 0 {
-			//Add it to the current value
-			index++
-			msb = getInt(track, index, index+1)
-			value = (value << 7) + msb&0x7F
-		}
-	}
-	return value, index + 1
-}
-
-/*
-	Returns an array of hex strings containing one track per string
-	If the MIDI file is in format 1, we ignore the first track
-*/
-func getTracks(hexMIDI string) ([]string, int) {
-	//Get information from the file header
-	length, format, numTracks := getHeaderInfo(hexMIDI)
-	fmt.Printf("Header Length: %d\nFormat: %d\nNumber of Tracks: %d\n", length, format, numTracks)
-	//We'll split the rest of the hex string into the tracks
-	//hexMIDI[(8+length)*2:)] gets rid of the header (length doesn't include the first 8 bytes
-	//Split after N uses the track end signifier to split the string into the remaining tracks
-	tracks := strings.SplitAfterN(hexMIDI[(8+length)*2:], trackEnd, numTracks)
-	//Format 0 specifies that only one track should exist
-	if format == 0 && len(tracks) != 1 {
-		fmt.Printf("Error\n")
-	} else if format == 1 {
-		//Format 1 says there's one or more tracks with an intial track containing the tempo info
-		//We don't need this info
-		tracks = tracks[1:]
-	}
-	//We don't need to do anything for format 2
-	return tracks, format
-}
-
-/* First character is index 0
- * Includes start, excludes end
- */
-func substring(s string, start, end int) string {
-	startIndex := start * 2
-	endIndex := end * 2
-	return s[startIndex:endIndex]
-}
-
-/*
-	Returns the integer specified by substring of the hex string
-*/
-func getInt(hexString string, start, end int) int {
-	num, err := strconv.ParseInt(substring(hexString, start, end), 16, 32)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return int(num)
-}
-
-/*
-	Returns information stored in header according to midi specifications
-	https://www.csie.ntu.edu.tw/~r92092/ref/midi/
-*/
-func getHeaderInfo(hexString string) (int, int, int) {
-	//We could use the first four bytes to check if its a valid MIDI file
-	//But I'm assuming it will work
-	length := getInt(hexString, 4, 8)
-	format := getInt(hexString, 8, 10)
-	numTracks := getInt(hexString, 10, 12)
-	return length, format, numTracks
 }
